0df22c577c3010b21e5fbeb491768c96cfeea3f7
Roshaanf <roshaan.farrukh@yahoo.com>
2018-04-16 12:14:02 +0500

    init2 commit

--- app/src/main/java/com/example/habibaabbasii/testcase/ui/activities/SplashActivity.java
+++ app/src/main/java/com/example/habibaabbasii/testcase/ui/activities/SplashActivity.java
@@ -26,9 +26,12 @@ public class SplashActivity extends AppCompatActivity {
 
                 if (mAuth.getCurrentUser() == null) {
                     startActivity(new Intent(SplashActivity.this, AuthenticationActivity.class));
+                    overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
                     finish();
                 } else {
                     startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                    overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+
                     finish();
                 }
             }
